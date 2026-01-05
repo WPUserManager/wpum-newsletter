183ee303cbe818a25e0bbe5cfb63e157659b79cb
Iain <iain@polevaultweb.com>
2026-01-05 08:11:07 +0000

    Add newsletter profile to account page

--- wpum-newsletter.php
+++ wpum-newsletter.php
@@ -37,3 +37,21 @@ function wpum_newsletter_init() {
 	add_filter( 'wpum_get_registration_fields', 'wpum_newsletter_add_optin' );
 }
 
+function wpum_newsletter_register_new_account_tabs( $tabs ) {
+	$tabs['posts'] = [
+		'name'     => esc_html( apply_filters( 'wpum_newsletters_account_title', __( 'Newsletters', 'wpum-newsletters' ) ) ),
+		'priority' => 0,
+	];
+
+	return $tabs;
+}
+
+add_filter( 'wpum_get_account_page_tabs', 'wpum_newsletter_register_new_account_tabs' );
+
+function wpum_newsletter_account_tab_content() {
+	echo '<h2>' . apply_filters( 'wpum_newsletters_account_title', __( 'Newsletters', 'wpum-newsletters' ) ) . '</h2>';
+	echo do_shortcode( '[newsletter_profile]' );
+}
+
+add_action( 'wpum_account_page_content_posts', 'wpum_newsletter_account_tab_content' );
+
